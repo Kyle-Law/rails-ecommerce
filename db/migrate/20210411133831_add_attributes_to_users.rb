d0cc0eeefefdf907fd3675fb5b9478d1cea834db
Kyle-Law <kylelaw1121@gmail.com>
2021-04-11 21:44:21 +0800

    FE1-Edit: Add address attribute to user

--- db/migrate/20210411133831_add_attributes_to_users.rb
+++ db/migrate/20210411133831_add_attributes_to_users.rb
@@ -3,6 +3,7 @@ class AddAttributesToUsers < ActiveRecord::Migration[6.0]
     add_column :users, :show_price, :boolean
     add_column :users, :credit, :decimal
     add_column :users, :phone, :string
+    add_column :users, :address, :string
     add_reference :users, :role, null: false, foreign_key: true
   end
 end
