b120a637b415dce3df19be20509c3bb660debd7d
Kyle-Law <kylelaw1121@gmail.com>
2021-04-11 22:03:31 +0800

    General-Edit: Set root path

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+  root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
