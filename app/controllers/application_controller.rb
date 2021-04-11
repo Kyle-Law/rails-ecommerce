b6dd20b47ffbd2934cd5f788a9c9b899bcd01624
Kyle-Law <kylelaw1121@gmail.com>
2021-04-11 22:23:47 +0800

    General-Add: Added header partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
   include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action. Please contact us at htthsupport@htgroup.com.my if you need any assistance."
+    redirect_to(request.referrer || root_path)
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,8 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :delivery_mode, required: false
   has_many :order_items, dependent: :destroy
+
+  def total_quantity
+    order_items.map(&:quantity).sum
+  end
 end
